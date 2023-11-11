302af3ec79301c0cbb2e02b4a49bd73075259c39
binoverfl0w <janidurres7@hotmail.com>
2023-11-11 20:25:38 +0100

    event model prototype

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { SQLiteService } from './db/sqlite.service';
 import { MongoDBService } from './db/mongodb.service';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, EventsModule],
   providers: [SQLiteService, MongoDBService],
 })
 export class AppModule {}
